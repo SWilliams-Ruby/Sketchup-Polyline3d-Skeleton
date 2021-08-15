d4aadbf3423bf3e1f5ef245858e9e6e29d1aad32
SWilliams-Ruby <88683212+SWilliams-Ruby@users.noreply.github.com>
2021-08-14 20:50:52 -0400

    Update skeleton.rb

--- src/sw_Skeleton/skeleton.rb
+++ src/sw_Skeleton/skeleton.rb
@@ -1,16 +1,16 @@
-# SW::Skeleton an extension to convert all of the edges in a selected group into
-# their equivalent sketchup polyline3d objects which will be ignored by Sketchup
-# inferencing algorithm(s). A side effect is that after this conversion the user
-# can no --- easily select---  longer apply a material to the group with the paint bucket.
-# to remedy this problem ------
-# paint
-# which can be accessed through the outliner, drag L-R, or edit menu
-#
-#
-# The entry point for the code is create_skeleton() where, we hope, the user
-# has selected a Sketchup::Group beforehand.
-#
-# behind the scenes//// or internally what we are doing
+# SW::Skeleton, an extension to convert all of the edges in 
+# a selected group into their equivalent sketchup polyline3d 
+# objects which will be ignored by the Sketchup inferencing 
+# algorithm(s). A side effect is that after this conversion 
+# the user can no longer easily apply a material to the group 
+# with the paint bucket. To remedy this problem select the 
+# skeletonized group in the outlliner or via a left to right 
+# select of the entire group.
+
+# Usage :
+# select a group
+# in the (right click) context menu choose Create Skeleton
+# to change the material, select the skeleton and in the (right clkick) context menu choose Paint Skeleton
 
 
 module SW
